325cc5a42eb8735d11d1b650416de910a6e170a8
Looking120 <joshndayi43@icloud.com>
2024-06-24 02:56:12 +0300

    changement de la page login et autre

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,17 +2,19 @@ import React from 'react';
 import './home.css';
 import SocialSection from './SocialSection';
 import HomeUs from './HomeUs';
+import DownScroll from './DownScroll';
 
 const Home = () => {
     return (
-        <section className="home section">
-            <div className="home-container container gap">
+        <section className="home section" id="home">
+            <div className="home-container container grid">
                 <div className="home-contain grid">
                     <SocialSection />
 
                     <div className="img-home"></div>
                     <HomeUs />
                 </div>
+                <DownScroll />
             </div>
         </section>
     );
